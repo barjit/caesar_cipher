f8ad3c672ff24d9bdb9e520f4c9f8c9ba003e0c9
jitterboggle <jeffreybartels@gmail.com>
2015-11-27 17:12:55 +1100

    Improve functionality

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,19 +1,39 @@
 def caesar_cipher(string, shift)
-  #split the string parameter into array of characters:
+
+  alphabet = [*('a'..'z'), *('A'..'Z')]
+  
+#split the string parameter into array of characters:
   split_string = string.chars
-
-  #iterate through each character and convert it to it's ascii number:
-  ascii = split_string.map {|ch| ch.ord}
-
-  #iterate through each ascii number and add the shift parameter to it:
-  shifted = ascii.map {|ascii| ascii + shift}
   
-  #iterate through each (now modified) ascii number and convert it back to a string character:
-  convert = shifted.map {|num| num.chr}
-  
-  #join array of string characters to make the new word:
+#iterate through each character and convert it to it's ascii number if it is a letter:
+  ascii = split_string.map do |ch|
+    if alphabet.include?(ch)
+      ch.ord
+    else
+      ch
+    end
+  end
+
+#iterate through each ascii number and add the shift parameter to it if it is an integer:
+  shifted = ascii.map do |x|
+    if x.is_a? Integer
+      x + shift
+    else
+      x
+    end
+  end
+#iterate through each (now modified) ascii number and convert it back to a string character:
+  convert = shifted.map do |y|
+    if y.is_a? Integer
+      y.chr
+    else
+      y
+    end
+  end
+
+#join array of string characters to make the new word:
   result = convert.join("")
-  
-  #puts the new word:
+
   puts "#{result}"
+
 end
