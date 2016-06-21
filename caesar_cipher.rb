ad44a935606e0939f60e83c04b37e0d8d19314dd
jitterboggle <jeffreybartels@gmail.com>
2016-06-21 19:18:48 +1000

    redo project

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,53 +1,29 @@
 def caesar_cipher(string, shift)
+  result = []
 
-  alphabet = [*('a'..'z'), *('A'..'Z')]
-  
-#split the string parameter into array of characters:
-  split_string = string.chars
-  
-#iterate through each character and convert it to it's ascii number if it is a letter:
-  ascii = split_string.map do |ch|
-    if alphabet.include?(ch)
-      ch.ord
-    else
-      ch
-    end
-  end
-
-#iterate through each ascii number and add the shift parameter to it if it is an integer:
-  shifted = ascii.map do |x|
-    if x.is_a? Integer
-      x + shift
-    else
-      x
-    end
-  end
-
-#iterate through each character, if it is a number check the ascii range it is in and subtract 26 if it is not a letter anymore.
-  loop_back = shifted.map do |z|
-    if z.is_a? Integer
-      if (90..96) === z
-        z - 26
-      elsif (z > 122)
-        z - 26
+  split_string = string.split("")
+  split_string.each do |character|
+    hex = character.ord
+    if (97..122).include? hex
+      shifted = hex + shift
+      if shifted > 122
+        rotate = (shifted - 122) + 96
+        result << rotate.chr
       else
-        z
+        result << shifted.chr
+      end
+    elsif (65..90).include? hex
+      shifted = hex + shift
+      if shifted > 90
+        rotate = (shifted - 90) + 64
+        result << rotate.chr
+      else
+        result << shifted.chr
       end
     else
-      z
-    end
-  end   
-
-
-#iterate through each (now modified) ascii number and convert it back to a string character:
-  convert = loop_back.map do |y|
-    if y.is_a? Integer
-      y.chr
-    else
-      y
+      result << hex.chr
     end
   end
-
-#join array of string characters to make the new word:
-  result = convert.join("")
+  result.join
+        
 end
